55bcfd8e1d215ece0e11dbac2267048ef325890b
JelenaVukojevic <jelena.vukojevic@live.com>
2020-07-31 02:52:14 +0200

    edit task

--- timesheet.php
+++ timesheet.php
@@ -5,7 +5,7 @@ if(isset($_GET['api_key']) && $_GET['api_key'] == $api_key) {
     $action = $_GET['action'];
     switch($action) {
         case 'getTasks':
-            $taskQuery = "SELECT d.id AS date_id, d.date, t.id, t.title, t.hours FROM dates AS d JOIN tasks AS t ON d.id = t.date_id WHERE date LIKE '" . $_GET['date'] . "'";
+            $taskQuery = "SELECT t.id, t.title, t.hours FROM dates AS d JOIN tasks AS t ON d.id = t.date_id WHERE date LIKE '" . $_GET['date'] . "'";
             $result = mysqli_query($conn, $taskQuery);
             $response = [];
             if ($result->num_rows > 0) {
@@ -15,17 +15,18 @@ if(isset($_GET['api_key']) && $_GET['api_key'] == $api_key) {
             }
             
             echo json_encode($response);
-        break;
+            break;
         case 'addTask':
-            $taskQuery = "SELECT d.id AS date_id, d.date, t.id, t.title, t.hours FROM dates AS d JOIN tasks AS t ON d.id = t.date_id WHERE date LIKE '" . $_GET['date'] . "'";
-            $result = mysqli_query($conn, $taskQuery);
-            $totalHours = 0;
+            $dateQuery = "SELECT id FROM dates WHERE date LIKE '" . $_GET['date'] . "'";
+            $result = mysqli_query($conn, $dateQuery);
             if ($result->num_rows > 0) {
                 while($row = mysqli_fetch_assoc($result)) {
-                    $dateID = $row['date_id'];
-                    $totalHours += $row['hours'];
+                    $dateID = $row['id'];
                 }
-                if (($totalHours + $_GET['hours']) > $allowedHours) {
+                $hoursQuery = "SELECT SUM(hours) AS total_hours from tasks WHERE date_id = " . $dateID;
+                $hours = mysqli_query($conn, $hoursQuery);
+                $totalHours = mysqli_fetch_assoc($hours);
+                if (($totalHours['total_hours'] + $_GET['hours']) > $allowedHours) {
                     echo json_encode(false);
                 } else {
                     $insertQuery = "INSERT INTO tasks VALUES (NULL, ".$dateID.", '".$_GET['title']."', ".$_GET['hours'].", now(), NULL)";
@@ -41,6 +42,18 @@ if(isset($_GET['api_key']) && $_GET['api_key'] == $api_key) {
                 mysqli_query($conn, $insertQuery);
                 $taskId = mysqli_insert_id($conn);
                 echo json_encode($taskId);
+            }
+            break;
+        case 'editTask' :
+            $hoursQuery = "SELECT SUM(hours) AS total_hours from tasks join dates ON tasks.date_id = dates.id WHERE dates.date LIKE '" . $_GET['date'] . "' AND tasks.id <> " . $_GET['id'];
+            $hours = mysqli_query($conn, $hoursQuery);
+            $totalHours = mysqli_fetch_assoc($hours);
+            if (($totalHours['total_hours'] + $_GET['hours']) > $allowedHours) {
+                echo json_encode(false);
+            } else {
+                $updateQuery = "UPDATE tasks SET title = '" . $_GET['title'] . "', hours = '" . $_GET['hours'] . "' WHERE id = " . $_GET['id'];
+                mysqli_query($conn, $updateQuery);
+                echo true;
             }
             break;
         case 'deleteTask':
