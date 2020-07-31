8bc66368cb5d4c2f1dc1e8631242ffdcc52709ea
JelenaVukojevic <jelena.vukojevic@live.com>
2020-07-31 15:32:52 +0200

    fix errors

--- timesheet.php
+++ timesheet.php
@@ -13,8 +13,6 @@ if(isset($_GET['api_key']) && $_GET['api_key'] == $api_key) {
                     $response[] = $row;
                 }
             }
-            
-            echo json_encode($response);
             break;
         case 'addTask':
             $dateQuery = "SELECT id FROM dates WHERE date LIKE '" . $_GET['date'] . "'";
@@ -23,44 +21,52 @@ if(isset($_GET['api_key']) && $_GET['api_key'] == $api_key) {
                 while($row = mysqli_fetch_assoc($result)) {
                     $dateID = $row['id'];
                 }
-                $hoursQuery = "SELECT SUM(hours) AS total_hours from tasks WHERE date_id = " . $dateID;
-                $hours = mysqli_query($conn, $hoursQuery);
-                $totalHours = mysqli_fetch_assoc($hours);
-                if (($totalHours['total_hours'] + $_GET['hours']) > $allowedHours) {
-                    echo json_encode(false);
+                $hoursQuery = "SELECT SUM(hours) AS hours_sum FROM tasks WHERE date_id = " . $dateID;
+                if ((getHoursSum($hoursQuery) + $_GET['hours']) > $allowedHours) {
+                    $response = false;
                 } else {
                     $insertQuery = "INSERT INTO tasks VALUES (NULL, ".$dateID.", '".$_GET['title']."', ".$_GET['hours'].", now(), NULL)";
                     mysqli_query($conn, $insertQuery);
                     $taskId = mysqli_insert_id($conn);
-                    echo json_encode($taskId);
+                    $response = $taskId;
                 }
             } else {
-                $insertQuery = "INSERT INTO dates VALUES (NULL, ".$_GET['date'].")";
-                mysqli_query($conn, $insertQuery);
-                $dateID = mysqli_insert_id($conn);
-                $insertQuery = "INSERT INTO tasks VALUES (NULL, ".$dateID.", '".$_GET['title']."', ".$_GET['hours'].", now(), NULL)";
-                mysqli_query($conn, $insertQuery);
-                $taskId = mysqli_insert_id($conn);
-                echo json_encode($taskId);
+                if($_GET['hours'] < $allowedHours) {
+                    $insertQuery = "INSERT INTO dates VALUES (NULL, '".$_GET['date']."')";
+                    mysqli_query($conn, $insertQuery);
+                    $dateID = mysqli_insert_id($conn);
+                    $insertQuery = "INSERT INTO tasks VALUES (NULL, ".$dateID.", '".$_GET['title']."', ".$_GET['hours'].", now(), NULL)";
+                    mysqli_query($conn, $insertQuery);
+                    $taskId = mysqli_insert_id($conn);
+                    $response = $taskId;
+                } else {
+                    $response = false;
+                }
             }
             break;
         case 'editTask' :
-            $hoursQuery = "SELECT SUM(hours) AS total_hours from tasks join dates ON tasks.date_id = dates.id WHERE dates.date LIKE '" . $_GET['date'] . "' AND tasks.id <> " . $_GET['id'];
-            $hours = mysqli_query($conn, $hoursQuery);
-            $totalHours = mysqli_fetch_assoc($hours);
-            if (($totalHours['total_hours'] + $_GET['hours']) > $allowedHours) {
-                echo json_encode(false);
+            $hoursQuery = "SELECT SUM(hours) AS hours_sum from tasks join dates ON tasks.date_id = dates.id WHERE dates.date LIKE '" . $_GET['date'] . "' AND tasks.id <> " . $_GET['id'];
+            if ((getHoursSum($hoursQuery) + $_GET['hours']) > $allowedHours) {
+                $response = false;
             } else {
                 $updateQuery = "UPDATE tasks SET title = '" . $_GET['title'] . "', hours = '" . $_GET['hours'] . "' WHERE id = " . $_GET['id'];
                 mysqli_query($conn, $updateQuery);
-                echo true;
+                $response = true;
             }
             break;
         case 'deleteTask':
             $deleteQuery = "DELETE FROM tasks WHERE id = " . $_GET['id'];
             mysqli_query($conn, $deleteQuery);
-            echo json_encode(true);
+            $response = true;
         break;
     }
+
+    echo json_encode($response);
+}
+
+function getHoursSum($query) {
+    $hours = mysqli_query($conn, $query);
+    $hoursSum = mysqli_fetch_assoc($hours);
+    return $hoursSum;
 }
 ?>
